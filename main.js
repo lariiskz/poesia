06a93ebb1714b7871ec84e6c6896e71697dd7bb5
lariiskz <127518749+lariiskz@users.noreply.github.com>
2024-10-18 22:12:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("mudarTexto").addEventListener("click", function() {
+    const poesia = document.getElementById("poesia");
+    poesia.textContent = "A liberdade mora no peito, \n" +
+                         "É caminhar sob o sol perfeito. \n" +
+                         "Viver por si, sem prisões, \n" +
+                         "Ouvir apenas as próprias razões.";
+});
